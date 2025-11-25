ea46f24ab8710bd0b3994c85205eea728136e7b4
EvgenyPinezhanin <pinezhanin@yandex.ru>
2025-11-26 00:39:20 +0300

    Added sample for model

---
+++ samples/sample_modeling.cpp
@@ -0,0 +1,26 @@
+﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// Copyright (c) Пинежанин Е.С.
+//
+// Тестирование модели вычислительного центра
+
+#include <iostream>
+
+#include "tcomputecentermodel.h"
+
+int main()
+{
+  size_t CountCycles = 1000;
+  size_t QueueSize = 16;
+  double q1 = 0.3;
+  double q2 = 0.25;
+
+  TComputeCenterModel model(CountCycles = 100, QueueSize, q1, q2);
+  model.Model();
+
+  setlocale(LC_ALL, "Russian");
+
+  model.PrintResult();
+
+  return 0;
+}
