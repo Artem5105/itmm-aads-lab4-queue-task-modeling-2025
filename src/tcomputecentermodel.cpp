98bdca15b4c04a835d76027e211d606080b63290
EvgenyPinezhanin <pinezhanin@yandex.ru>
2025-11-26 00:39:09 +0300

    Added templates for queue and model

---
+++ src/tcomputecentermodel.cpp
@@ -0,0 +1,32 @@
+// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
+//
+// tcomputecentermodel.cpp
+//
+// Copyright (c) Пинежанин Е.С.
+
+#include <tcomputecentermodel.h>
+
+double GetRandomValue() // вернуть случайное значение от 0.0 до 1.0
+{
+
+}
+
+TComputeCenterModel::TComputeCenterModel(size_t _CountCycles, size_t _QueueSize, double _q1, double _q2)
+{
+
+}
+
+void TComputeCenterModel::SetParameters(size_t _CountCycles, size_t _QueueSize, double _q1, double _q2)
+{
+
+}
+
+void TComputeCenterModel::Model()
+{
+
+}
+
+void TComputeCenterModel::PrintResult() const
+{
+
+}
